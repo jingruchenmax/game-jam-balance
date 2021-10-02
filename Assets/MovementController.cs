2605375a2b8b87d2da9c896e45c01b7020e8348d
Mirroooor <jchen19@wpi.edu>
2021-10-02 18:46:01 -0400

    initial summit

---
+++ Assets/MovementController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.GetComponent<Rigidbody>()!=null)
+            rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, 1),ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, -1), ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddTorque(new Vector3(0, 0, 0), ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddTorque(new Vector3(0, 0, 0), ForceMode.Impulse);
+        }
+    }
+}
